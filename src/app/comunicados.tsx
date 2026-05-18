06670291c0756ca1548d86602658ee3087e9a1dc
Thereal <phbossosouza2009@gmail.com>
2026-05-18 12:19:18 -0300

    atualização 1

---
+++ src/app/comunicados.tsx
@@ -0,0 +1,9 @@
+import { Image, Pressable, StyleSheet, Text, View } from 'react-native';
+export default function Comunicados(){
+    return(
+        <View>
+            
+        </View>
+
+    )
+}
