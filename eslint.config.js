1cc0b17319d1a89fd94a2f0752c94e017ef7b1cc
PedroBosso <phbossosouza2009@gmail.com>
2026-09-23 20:40:02 -0300

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -7,4 +7,16 @@ module.exports = defineConfig([
   {
     ignores: ['dist/*'],
   },
+  {
+    // Regras do React Compiler que chegaram com o eslint-config-expo do SDK 56.
+    // Elas apontam o padrão de buscar dados no useEffect usado em todas as
+    // telas: como a gravação do estado acontece depois de um await, a regra não
+    // consegue verificar e assume o pior. São sugestões de otimização, não erros
+    // de funcionamento.
+    rules: {
+      'react-hooks/immutability': 'off',
+      'react-hooks/refs': 'off',
+      'react-hooks/set-state-in-effect': 'off',
+    },
+  },
 ]);
